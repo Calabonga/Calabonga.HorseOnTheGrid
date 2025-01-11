4b471ec824ed30c15330c6554433be98821a68c3
Sergei Calabonga <dev@calabonga.net>
2025-01-11 11:01:27 +0700

    New project created

---
+++ src/WpfMvv/Calabonga.HorseOnTheGridMvvm/Calabonga.HorseOnTheGridMvvm/Core/DependencyContainer.cs
@@ -0,0 +1,27 @@
+﻿using Calabonga.HorseOnTheGridMvvm.ViewModels;
+using Calabonga.HorseOnTheGridMvvm.Views;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Calabonga.HorseOnTheGridMvvm.Core
+{
+    internal static class DependencyContainer
+    {
+        internal static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddLogging(options =>
+            {
+                options.AddSerilog(dispose: true);
+                options.AddDebug();
+            });
+
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<MainWindowViewModel>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
