ef88f811c7d0b9f720fd3620d58c0ca8eb0669a6
Sergei Calabonga <dev@calabonga.net>
2025-01-18 10:04:23 +0700

    Draw board after setup

--- src/WpfMvv/Calabonga.HorseOnTheGridMvvm/Calabonga.HorseOnTheGridMvvm/Game/HorseGame.cs
+++ src/WpfMvv/Calabonga.HorseOnTheGridMvvm/Calabonga.HorseOnTheGridMvvm/Game/HorseGame.cs
@@ -1,9 +1,34 @@
-﻿namespace Calabonga.HorseOnTheGridMvvm.Game;
+﻿using Calabonga.HorseOnTheGridMvvm.Game.Helpers;
+using Calabonga.HorseOnTheGridMvvm.UserControls;
+
+namespace Calabonga.HorseOnTheGridMvvm.Game;
 
 public class HorseGame
 {
     public HorseGame(int rows, int columns)
     {
+        PrepareCells(rows, columns);
+    }
 
+    public IList<GridCell> GridCells { get; private set; } = [];
+
+    public int Score { get; private set; } = 1;
+
+    private void PrepareCells(int rows, int columns)
+    {
+        for (var x = 0; x < rows; x++)
+        {
+            for (var y = 0; y < columns; y++)
+            {
+                var cell = new GridCell
+                {
+                    ImageSource = ImageLoader.Empty,
+                    Title = x == 0 && y == 0 ? "1" : "",
+                    Row = x,
+                    Column = y
+                };
+                GridCells.Add(cell);
+            }
+        }
     }
 }
--- src/WpfMvv/Calabonga.HorseOnTheGridMvvm/Calabonga.HorseOnTheGridMvvm/ViewModels/MainWindowViewModel.cs
+++ src/WpfMvv/Calabonga.HorseOnTheGridMvvm/Calabonga.HorseOnTheGridMvvm/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,118 @@
 ﻿using Calabonga.HorseOnTheGridMvvm.Core;
+using Calabonga.HorseOnTheGridMvvm.Game;
+using Calabonga.HorseOnTheGridMvvm.UserControls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 
-namespace Calabonga.HorseOnTheGridMvvm.ViewModels
+namespace Calabonga.HorseOnTheGridMvvm.ViewModels;
+
+/// <summary>
+/// ViewModel for MainWindow
+/// </summary>
+public partial class MainWindowViewModel : ViewModelBase
 {
-    /// <summary>
-    /// ViewModel for MainWindow
-    /// </summary>
-    public partial class MainWindowViewModel : ViewModelBase
+    private HorseGame? _game;
+
+    public MainWindowViewModel()
     {
-        public MainWindowViewModel()
+        Title = "HORSE ON THE GRID (MVVM)";
+
+    }
+
+    #region properties
+
+    #region property CanShowBoard
+
+    public bool CanShowBoard => !IsGameNotRunning || _game?.Score > 0;
+
+    #endregion
+
+    #region property IsGameNotRunning
+
+    public bool IsGameNotRunning => !IsGameRunning;
+
+    #endregion
+
+    #region property IsGameRunning
+
+    /// <summary>
+    /// Property IsGameRunning
+    /// </summary>
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsGameNotRunning))]
+    [NotifyPropertyChangedFor(nameof(CanShowBoard))]
+    private bool _isGameRunning;
+
+    #endregion
+
+    #region property Cells
+
+    /// <summary>
+    /// Property Cells
+    /// </summary>
+    [ObservableProperty]
+    private ObservableCollection<GridCell> _cells = [];
+
+    #endregion
+
+    #region property Rows
+
+    /// <summary>
+    /// Property Rows
+    /// </summary>
+    [ObservableProperty]
+    private int _rows = 10;
+
+    #endregion
+
+    #region property Columns
+
+    /// <summary>
+    /// Property Columns
+    /// </summary>
+    [ObservableProperty]
+    private int _columns = 10;
+
+    #endregion
+
+    #endregion
+
+    #region commands
+
+    #region command StartGameCommand
+
+    [RelayCommand]
+    private void StartGame()
+    {
+        if (!IsGameRunning)
         {
-            Title = "HORSE ON THE GRID (MVVM)";
+            IsGameRunning = true;
         }
+
+        RunGame();
+
+    }
+    #endregion
+
+    #endregion
+
+    private void RunGame()
+    {
+        SetupGrid();
+
+        //Draw();
+    }
+
+    private void SetupGrid()
+    {
+        if (Cells.Count > 0)
+        {
+            Cells.Clear();
+        }
+
+        _game = new HorseGame(Rows, Columns);
+
+        Cells = new ObservableCollection<GridCell>(_game.GridCells);
     }
 }
