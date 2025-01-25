dda2c702dc84045ab45cc1215a5c969a7e3db3f3
Sergei Calabonga <dev@calabonga.net>
2025-01-25 10:14:57 +0700

    mvvm-video3

--- src/WpfMvv/Calabonga.HorseOnTheGridMvvm/Calabonga.HorseOnTheGridMvvm/Game/Extensions/GridCellExtensions.cs
+++ src/WpfMvv/Calabonga.HorseOnTheGridMvvm/Calabonga.HorseOnTheGridMvvm/Game/Extensions/GridCellExtensions.cs
@@ -1,6 +1,45 @@
-﻿namespace Calabonga.HorseOnTheGridMvvm.Game.Extensions;
+﻿using Calabonga.HorseOnTheGridMvvm.Game.Entities;
+using Calabonga.HorseOnTheGridMvvm.UserControls;
+
+namespace Calabonga.HorseOnTheGridMvvm.Game.Extensions;
 
 public static class GridCellExtensions
 {
+    public static IEnumerable<GridCell> FindDirectionMoves(this IList<GridCell> source)
+    {
+        var directions = Direction.Moves();
+        var emptyPosition = source.EmptyPositions().ToList();
+        var activePosition = source.GetActiveCell();
+
+        foreach (var direction in directions)
+        {
+            var position = activePosition.CheckDirections(direction, emptyPosition);
+            if (position == null)
+            {
+                continue;
+            }
+
+            yield return position;
+        }
+    }
+
+    internal static GridCell GetActiveCell(this IList<GridCell> source)
+    {
+        return source.First(x => x.CellType is CellType.Active);
+    }
+
+    private static IEnumerable<GridCell> EmptyPositions(this IList<GridCell> source)
+    {
+        return source.Where(x => x.CellType is CellType.Empty or CellType.Available);
+    }
+
+    private static GridCell? CheckDirections(this GridCell source, Direction direction,
+        IEnumerable<GridCell> emptyPositions)
+    {
+        var row = source.Row + direction.RowOffset;
+        var column = source.Column + direction.ColumnOffset;
+
+        return emptyPositions.FirstOrDefault(x => x.Row == row && x.Column == column);
+    }
 
 }
